ce9efd02d1f821a3054ffa0867a9373fdf90dd11
Alekhyaa <hialekhyaa@gmail.com>
2024-10-29 14:07:03 -0500

    Main menu template

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() {
+        //SceneManager.LoadScene(" add name of next scene here");
+        //Add Scenes to build
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
