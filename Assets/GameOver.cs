f4de985821b04c9563c419de3b82b52b62364799
Alekhyaa <hialekhyaa@gmail.com>
2024-11-06 12:44:44 -0600

    Game over template

---
+++ Assets/GameOver.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void RestartGame() {
+        //SceneManager.LoadScene(" add name of play scene here");
+        //Add Scenes to build
+    }
+    public void MainMenuScreen() {
+        //SceneManager.LoadScene(" add name of main menu scene here");
+        //Add Scenes to build
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
