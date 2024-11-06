aca09d098ba45a266018556ad5e5f9ac36a492a1
annask817 <annasuek817@gmail.com>
2024-11-06 14:57:11 -0600

    Actual level scene

--- Assets/Boundary.cs
+++ Assets/Boundary.cs
@@ -7,34 +7,10 @@ public class Boundary : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Player"))
+        if (other.CompareTag("Destroyable"))
         {
             Destroy(other.gameObject);
         }
-        /*
-        else
-        {
-            Rigidbody rb = other.GetComponent<Rigidbody>();
-            if(rb != null)
-            {
-                rb.velocity = Vector3.zero;
-            }
-            // Alternatively, move the object back within the boundary
-            Vector3 boundaryPosition = transform.position;
-            Vector3 newPosition = other.transform.position;
-
-            // Adjust the position if the object goes out of bounds (adjust axis as needed)
-            if (newPosition.x > boundaryPosition.x)
-            {
-                newPosition.x = boundaryPosition.x;
-            }
-            if (newPosition.z > boundaryPosition.z)
-            {
-                newPosition.z = boundaryPosition.z;
-            }
-
-            other.transform.position = newPosition;
-        }*/
     }
 
     // Start is called before the first frame update
