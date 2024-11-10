f48d2da0f391a4b81c0267bf9fb41584cdec78ae
5sidd <siddpcal@gmail.com>
2024-11-09 23:56:17 -0600

    Added vehicle respawning functionality after collision with boundary.

--- Assets/Boundary.cs
+++ Assets/Boundary.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Boundary : MonoBehaviour
 {
+    public GameObject vehiclePrefab;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Destroyable"))
         {
-            Destroy(other.gameObject);
+            Debug.Log("Car collided with boundary!");
+            // Debug.Log(other.gameObject.GetComponent<MovingCar>());
+            // other.gameObject.SetActive(false);
+            // Destroy(other.gameObject);
+            // Vector3 position = new Vector3(-17.82f, 2.342f, 0.59f);
+            // GameObject newVehicle = Instantiate(vehiclePrefab, Vector3.zero, Quaternion.identity);
+            // MovingCar carScript = newVehicle.GetComponent<MovingCar>();
+            // Vector3 vehiclePosition = carScript.originalPosition;
+            // newVehicle.transform.position = vehiclePosition;
         }
     }
 
