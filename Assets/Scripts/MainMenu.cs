addb9f07e6bd76018863612e96efa0fd30e6d51c
5sidd <siddpcal@gmail.com>
2024-11-19 18:04:04 -0600

    Fixed MainMenu scene names and exit game functionality

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame() {
-        SceneManager.LoadScene("LevelStart");
+        SceneManager.LoadScene("LevelwithEnvironment");
     }
 
     public void QuitGame() {
-        Application.Quit();
+        #if UNITY_EDITOR
+        EditorApplication.isPlaying = false; // Stops play mode in the editor
+        #endif        
+        //Application.Quit();
     }
 }
