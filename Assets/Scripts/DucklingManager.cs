797a48a0de1b294a09e9f560b35a49619bd41ecf
AnnaSK <144516813+annask817@users.noreply.github.com>
2024-11-21 09:41:10 -0600

    duck spawner + placehold counter

--- Assets/Scripts/DucklingManager.cs
+++ Assets/Scripts/DucklingManager.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DucklingManager : MonoBehaviour
 {
     public int ducklingCount = 0;
+    public Text duckCountText;
     [SerializeField] private GameObject playerDuckPrefab;
 
     void Start()
     {
         if (gameObject.name != "DucklingManager")
         {
-            Debug.LogWarning("DucklingManager should be on the GameManager object!");
+            Debug.LogWarning("DucklingManager should be on the DucklingManager object!");
+        }
+    }
+
+    void Update()
+    {
+        duckCountText.text = "Duckling Count: " + ducklingCount.ToString();
+        if (ducklingCount == 5)
+        {
+            // end game
         }
     }
 
